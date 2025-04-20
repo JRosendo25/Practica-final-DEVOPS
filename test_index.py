8bc441069a2a75e0420a551aa349d3a4b30bd2f6
JRosendo25 <j.jefferrosendofelix@gmail.com>
2025-04-20 00:04:05 -0400

    Update test_index.py

--- test_index.py
+++ test_index.py
@@ -1,8 +1,4 @@
-from bs4 import BeautifulSoup
-
 def test_hola_mundo():
-    with open("index.html", "r", encoding="utf-8") as file:
-        soup = BeautifulSoup(file, "html.parser")
-        h1 = soup.find("h1")
-        assert h1 is not None
-        assert "Hola Mundo" in h1.text
+    with open("src/index.html", encoding="utf-8") as f:
+        contenido = f.read()
+    assert "<h1>¡Hola Mundo!</h1>" in contenido
